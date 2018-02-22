83d928b1a65d8210168e280eae68a85d430ce20d
Natthakit <nonusae@gmail.com>
2018-02-22 17:23:51 +0700

    Perform a neatly refactor all code base :)

--- js/main.js
+++ js/main.js
@@ -3,126 +3,87 @@ const track = document.querySelector('.jsTrack')
 const slides = Array.from(track.children)
 const slideWidth = slides[0].getBoundingClientRect().width
 const dotContainer = document.querySelector('.jsDotContainer')
+const dots = dotContainer.children;
 const nextButton = document.querySelector('.jsNext');
 const previousButton = document.querySelector('.jsPrevious');
-
 
 slides.forEach((slide,index) => {
   slide.style.left = (slideWidth * index) + 'px'
 })
 
-// next button
-
-nextButton.addEventListener('click', e => {
-  previousButton.classList.remove('is-hidden');
-
-  let currentSlide
-
-  for(slide of slides) {
-    if (slide.classList.contains('is-selected')){
-      currentSlide = slide
-    }
-  }
-  const nextSlide = currentSlide.nextElementSibling
-  const amountToMove = nextSlide.style.left;
-  track.style.left = '-' + amountToMove
-  currentSlide.classList.remove('is-selected')
-  nextSlide.classList.add('is-selected')
-
-  const isFinalSlide = !nextSlide.nextElementSibling;
-  if (isFinalSlide){
-    nextButton.classList.add('is-hidden')
-  }
-
-  const currentDot = dotContainer.querySelector('.is-selected')
-  const nextDot = currentDot.nextElementSibling
-
-  currentDot.classList.remove('is-selected')
-  nextDot.classList.add('is-selected')
-
-})
-
-
-// previous button
-previousButton.addEventListener('click', e => {
-  nextButton.classList.remove('is-hidden')
-
-  let currentSlide
-
-  for(let slide of slides ) {
-    if (slide.classList.contains('is-selected')){
-      currentSlide = slide
-    }
-  }
-
-  const previousSlide = currentSlide.previousElementSibling
-  const amountToMove = previousSlide.style.left
-  track.style.left = '-' + amountToMove
-
-  currentSlide.classList.remove('is-selected')
-  previousSlide.classList.add('is-selected')
-
-  const currentDot = dotContainer.querySelector('.is-selected');
-  const previousDot = currentDot.previousElementSibling;
-
-  currentDot.classList.remove('is-selected');
-  previousDot.classList.add('is-selected');
-
-  const isFirstSlide = !previousSlide.previousElementSibling
-
-  if (isFirstSlide) {
-    previousButton.classList.add('is-hidden')
-  }
-
-  })
-
-
-// Dots
-dotContainer.addEventListener('click', e => {
-  if (!e.target.matches('button')) return
-
-  let currentSlide
-
-  for(let slide of slides ) {
-    if (slide.classList.contains('is-selected')){
-      currentSlide = slide
-    }
-  }
-// Find index of clicked dot
-  const clickedDot = e.target
-  dots = dotContainer.children
-  let targetIndex
-
-  for(let index=0; index < dots.length; index ++ ){
-    if (dots[index] === clickedDot){
-      targetIndex = index
-    }
-  }
-
-// Update slide to targetIndex
+const updateSlides = (track,currentIndex,targetIndex) => {
+  const currentSlide = slides[currentIndex]
   const targetSlide = slides[targetIndex]
-  const amountToMove = targetSlide.style.left;
+  track.style.left = '-' + targetSlide.style.left;
   currentSlide.classList.remove('is-selected')
   targetSlide.classList.add('is-selected')
-  track.style.left = '-' + amountToMove
+}
 
-// Update Dot
-  const currentDot = dotContainer.querySelector('.is-selected');
-  currentDot.classList.remove('is-selected');
-  clickedDot.classList.add('is-selected');
+const updateDots = (currentIndex,targetIndex) => {
+  dots[currentIndex].classList.remove('is-selected');
+  dots[targetIndex].classList.add('is-selected');
+}
 
-// Update arrow
-  const isFinalDotClicked = ( targetIndex === slides.length - 1 )
-  const isFirstDotClicked = ( targetIndex === 0 )
-
-  if (isFinalDotClicked) {
-    nextButton.classList.add('is-hidden')
-    previousButton.classList.remove('is-hidden')
-  }else if (isFirstDotClicked){
-    previousButton.classList.add('is-hidden')
-    nextButton.classList.remove('is-hidden')
-  }else{
-    nextButton.classList.remove('is-hidden')
-    previousButton.classList.remove('is-hidden')
+const updateArrows = targetIndex => {
+  if (targetIndex === slides.length - 1) {
+    nextButton.classList.add('is-hidden');
+    previousButton.classList.remove('is-hidden');
+  } else if ( targetIndex === 0 ) {
+    previousButton.classList.add('is-hidden');
+    nextButton.classList.remove('is-hidden');
+  } else {
+    nextButton.classList.remove('is-hidden');
+    previousButton.classList.remove('is-hidden');
   }
-})
+}
+
+const getCurrentIndex = slides => {
+  for(let index = 0 ; index < dots.length ; index++ ) {
+    const slide = slides[index]
+    if (slide.classList.contains('is-selected')){
+      return index
+    }
+  }
+}
+
+// Call back for button
+nextSlide = e => {
+  const currentIndex = getCurrentIndex(slides);
+  const nextIndex = currentIndex + 1;
+
+  updateSlides(track, currentIndex, nextIndex);
+  updateArrows(nextIndex);
+  updateDots(currentIndex, nextIndex);
+}
+
+previousSlide = e => {
+  const currentIndex = getCurrentIndex(slides);
+  const previousIndex = currentIndex - 1;
+
+  updateSlides(track, currentIndex, previousIndex);
+  updateArrows(previousIndex);
+  updateDots(currentIndex, previousIndex);
+}
+
+setSlide = e => {
+  if (!e.target.matches('button')) return;
+
+  const currentIndex = getCurrentIndex(slides);
+  const clickedDot = e.target;
+  let targetIndex;
+
+  for (let index = 0; index < dots.length; index++) {
+    if (dots[index] === clickedDot) {
+      targetIndex = index;
+    }
+  }
+
+  updateDots(currentIndex, targetIndex);
+  updateSlides(track, currentIndex, targetIndex);
+  updateArrows(targetIndex);
+}
+
+// EventListener
+nextButton.addEventListener('click',nextSlide)
+previousButton.addEventListener('click', previousSlide )
+dotContainer.addEventListener('click', setSlide )
